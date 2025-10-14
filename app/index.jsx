ea69a37c41e046a86b3b13090985bd4ceb1eb954
arijanabraha <arijana.braha@student.uni-pr.edu>
2025-10-14 14:56:23 +0200

    commit2

--- app/index.jsx
+++ app/index.jsx
@@ -1,46 +1,42 @@
-import { Ionicons } from '@expo/vector-icons'; // për ikonat (expo install @expo/vector-icons)
-import { Image, StyleSheet, TouchableOpacity, View } from 'react-native';
+import { Ionicons, MaterialCommunityIcons } from '@expo/vector-icons';
+import { Image, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-
-  
-      
       {/* Header */}
       <View style={styles.header}>
-
-
-
         <View style={styles.logoContainer}>
           <Image 
             source={require('../assets/images/logotransparente.png')} 
             style={styles.logoImage} 
             resizeMode="contain"
           />
-          
         </View>
-
-
-
-
         <TouchableOpacity onPress={() => alert('Settings clicked!')}>
           <Ionicons name="settings-outline" size={28} color="#000" />
         </TouchableOpacity>
       </View>
 
-
-      
-
-
-
-    
-
-      {/* Përmbajtja e app */}
+      {/* Përmbajtja */}
       <View style={styles.content}>
-        {/* Këtu vendos përmbajtjen e app */}
+        <Text>Hello</Text>
       </View>
 
+      {/* Footer */}
+      <View style={styles.footer}>
+        <TouchableOpacity onPress={() => alert('Home clicked!')}>
+          <Ionicons name="home-outline" size={28} color="#000" />
+        </TouchableOpacity>
+
+        <TouchableOpacity onPress={() => alert('Library clicked!')}>
+          <MaterialCommunityIcons name="book-open-outline" size={28} color="#000" />
+        </TouchableOpacity>
+
+        <TouchableOpacity onPress={() => alert('Search clicked!')}>
+          <Ionicons name="search-outline" size={28} color="#000" />
+        </TouchableOpacity>
+      </View>
     </View>
   );
 }
@@ -48,32 +44,39 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#dfc1d7ff',
+    backgroundColor: '#e5d0dfff',
+    justifyContent: 'space-between', // kjo bën që footer-i të shkojë në fund
   },
   header: {
     height: 60,
-    backgroundColor: '#dfc1d7ff',
-    flexDirection: 'row', // logo dhe ikonë në një rresht
-    alignItems: 'center', // qendër horizontalisht
-    justifyContent: 'space-between', // logo në të majtë, settings në të djathtë
+    backgroundColor: '#e5d0dfff',
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
     paddingHorizontal: 15,
-    shadowColor: "#000", // hije për iOS
+    shadowColor: "#000",
     shadowOffset: { width: 0, height: 2 },
     shadowOpacity: 0.2,
     shadowRadius: 2,
-    elevation: 5, // hije për Android
-  },
-  logo: {
-    fontSize: 20,
-    fontWeight: 'bold',
+    elevation: 5,
   },
   content: {
     flex: 1,
     padding: 20,
   },
-   logoContainer: { flexDirection: 'row', alignItems: 'center' },
-   logoImage: { width: 150, height: 100, borderRadius: 15, marginRight: 8 },
- 
-
-
+  footer: {
+    height: 60,
+    backgroundColor: '#e5d0dfff',
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    paddingHorizontal: 30,
+    shadowColor: "#000",
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.2,
+    shadowRadius: 2,
+    elevation: 5,
+  },
+  logoContainer: { flexDirection: 'row', alignItems: 'center' },
+  logoImage: { width: 150, height: 100, borderRadius: 15, marginRight: 8 },
 });
