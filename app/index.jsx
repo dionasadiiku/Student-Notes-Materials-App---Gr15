3a5d0ed5b0532ed93652f36e75d0bd10fa6c6c48
diellfazliu <diell.fazliu@student.uni-pr.edu>
2025-10-14 15:10:17 +0200

    favorite modal

--- app/index.jsx
+++ app/index.jsx
@@ -4,7 +4,6 @@ import { Image, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      {/* Header */}
       <View style={styles.header}>
         <View style={styles.logoContainer}>
           <Image 
@@ -18,12 +17,25 @@ export default function App() {
         </TouchableOpacity>
       </View>
 
-      {/* Përmbajtja */}
       <View style={styles.content}>
-        <Text>Hello</Text>
+        {/* <Text style={styles.sectionTitle}>My Studylist</Text>
+        <Text style={styles.subtitle}>Tap to learn more</Text> */}
+
+        <TouchableOpacity 
+          style={styles.favoriteCard}
+          onPress={() => alert('My Favorites clicked!')}
+          activeOpacity={0.8}
+        >
+          <View style={styles.iconContainer}>
+            <MaterialCommunityIcons name="bookmark-outline" size={24} color="#fff" />
+          </View>
+          <View>
+            <Text style={styles.favoriteTitle}>My favorites</Text>
+            <Text style={styles.favoriteSubtitle}>Tap to learn more</Text>
+          </View>
+        </TouchableOpacity>
       </View>
 
-      {/* Footer */}
       <View style={styles.footer}>
         <TouchableOpacity onPress={() => alert('Home clicked!')}>
           <Ionicons name="home-outline" size={28} color="#000" />
@@ -44,8 +56,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#e5d0dfff',
-    justifyContent: 'space-between', // kjo bën që footer-i të shkojë në fund
+    backgroundColor: '#fdfcff',
+    justifyContent: 'space-between',
   },
   header: {
     height: 60,
@@ -79,4 +91,43 @@ const styles = StyleSheet.create({
   },
   logoContainer: { flexDirection: 'row', alignItems: 'center' },
   logoImage: { width: 150, height: 100, borderRadius: 15, marginRight: 8 },
+  sectionTitle: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    marginBottom: 4,
+  },
+  subtitle: {
+    fontSize: 14,
+    color: '#777',
+    marginBottom: 16,
+  },
+  favoriteCard: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    backgroundColor: '#fde4cf',
+    borderRadius: 16,
+    padding: 16,
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 1 },
+    shadowOpacity: 0.2,
+    shadowRadius: 2,
+    elevation: 2,
+  },
+  iconContainer: {
+    width: 40,
+    height: 40,
+    borderRadius: 12,
+    backgroundColor: '#ff8fab',
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginRight: 12,
+  },
+  favoriteTitle: {
+    fontSize: 16,
+    fontWeight: '600',
+  },
+  favoriteSubtitle: {
+    fontSize: 13,
+    color: '#555',
+  },
 });
